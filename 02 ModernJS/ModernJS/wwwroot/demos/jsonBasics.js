82854adf46a8b8cad0a395d68145be64be51ef3a
Alexander Pajer <alexander.pajer@integrations.at>
2019-08-26 14:15:14 +0200

    fetch

--- 02 ModernJS/ModernJS/wwwroot/demos/jsonBasics.js
+++ 02 ModernJS/ModernJS/wwwroot/demos/jsonBasics.js
@@ -105,3 +105,13 @@ function JsonjQuery() {
     errHandler
   );
 }
+
+function usingFetch() {
+  fetch("/demos/vouchers.json")
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(myJson) {
+      console.log(JSON.stringify(myJson));
+    });
+}
