309834faa1cac366f5e5331861238579bbbc5b3b
Alexander Pajer <alexander.pajer@integrations.at>
2019-11-10 18:39:20 +0100

    restructure

--- 02 ModernJS/ModernJavaScript/demos/scriptToBeLoaded.js
+++
@@ -1,2 +0,0 @@
-﻿
-console.log("loaded script executed");
--- 02 ModernJS/ModernJavaScript/demos/systemjssample.js
+++
@@ -1,5 +0,0 @@
-System.import("./systemjsutils.js").then(exports => {
-  console.log("Imported:", exports);
-  let logger = exports.Logger;
-  logger.log("testing logger");
-});
