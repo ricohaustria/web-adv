7d4d20afc859c2c85ecc7a596b2fdf5168d1b96b
Alexander Pajer <alexander.pajer@integrations.at>
2020-07-12 12:39:39 +0200

    all upgrade

---
+++ 12-SecuritySocial/ngSkillsSecure/src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  token: 'QeOv74uiEDNz7QM91bjYKc82VgS2',
+  firebase: {
+    apiKey: 'AIzaSyAed-OAkXVgn_BHewFkPgUWQO8o1t-Kcuk',
+    authDomain: 'xskills-661db.firebaseapp.com',
+    databaseURL: 'https://xskills-661db.firebaseio.com',
+    projectId: 'xskills-661db',
+    storageBucket: '',
+    messagingSenderId: '308138681179',
+    appId: '1:308138681179:web:b7e4b089734417ff'
+  }
+};
