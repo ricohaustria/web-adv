3a09c6fe455f50be0681071aac60612c3b27e264
Alexander Pajer <alexander.pajer@integrations.at>
2021-12-01 07:16:00 +0100

    pwa update to 12

---
+++ Demos/09-Real Time-PWA/skills-pwa/src/app/skills/skills.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { SkillsService } from '../skills.service';
+import { Skill } from '../model';
+
+@Component({
+  selector: 'app-skills',
+  templateUrl: './skills.component.html',
+  styleUrls: ['./skills.component.scss'],
+})
+export class SkillsComponent implements OnInit {
+  skills: Skill[];
+  skillToAdd: string;
+
+  constructor(private service: SkillsService) {
+    this.skills = [];
+    this.skillToAdd = '';
+  }
+
+  ngOnInit() {
+    this.service.getSkills().subscribe((data) => (this.skills = data));
+  }
+
+  removeSkill(s: Skill) {
+    this.skills = this.skills.filter((i: Skill) => i !== s);
+  }
+
+  addSkill() {
+    let sk: Skill = { id: this.skills.length + 1, name: this.skillToAdd };
+    this.skills.push(sk);
+  }
+}
